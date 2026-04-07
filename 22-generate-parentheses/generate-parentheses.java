8cf255b37f267319e3c1413e824551d2dbf57e23
arshit aggarwal <arshit200515@gmail.com>
2026-04-07 18:03:40 +0530

    Time: 2 ms (71.98%) | Memory: 44.7 MB (60.84%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,25 +1,21 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        ArrayList <String> str = new ArrayList <>();
-        solve ("",0,0,n,str);
-        return str;
-  
-        
+        ArrayList <String> arr = new ArrayList <>();
+        solve (n, "", arr,0,0);
+        return arr;
+             
     }
-    void solve (String curr , int open , int close , int n , List<String> str){
-        if (curr.length()==2*n){
-            str.add(curr);
-            return ;
-
-        } 
+    void solve (int n, String str,List <String> arr ,int open ,int close){
+        if (str.length()==2*n){
+            arr.add(str);
+            return;
+        }
         if (open<n){
-            solve (curr+"(",open+1, close,n,str);
-
+            solve(n,str+"(",arr,open+1,close);
         }
-        if (open> close ){
-            solve (curr+")", open, close+1,n,str);
+        if(close<open){
+            solve(n,str+")",arr,open,close+1);
 
         }
     }
-
 }
