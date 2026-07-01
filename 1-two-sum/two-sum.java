aeeaf3574434bab927266e79cad513cf31b82940
arshit aggarwal <arshit200515@gmail.com>
2026-07-01 20:52:48 +0530

    Time: 49 ms (8.24%) | Memory: 47.2 MB (39.71%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -7,6 +7,7 @@ class Solution {
                 if (nums[i]+nums[j]==target){
                     ans[0]= i;
                     ans[1]= j;
+                    break;
                 }
             }
 
